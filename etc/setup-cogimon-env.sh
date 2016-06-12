af1978acb4261b67c26392cd83626ef737b22242
Sebastian Wrede <sebwrede@gmail.com>
2016-06-12 10:30:10 +0200

    removed setup script, moved to cogimon-scenarios

--- etc/setup-cogimon-env.sh
+++
@@ -1,5 +0,0 @@
-export GAZEBO_PLUGIN_PATH=/vol/toolkit/cogimon-minimal-nightly/lib/orocos/gnulinux/RTTGazeboClockPlugin
-export GAZEBO_MODEL_PATH=/vol/toolkit/cogimon-minimal-nightly/share/gazebo/models/cogimon
-export RTT_COMPONENT_PATH=/vol/toolkit/cogimon-minimal-nightly/lib/orocos
-export PATH=$PATH:/vol/toolkit/cogimon-minimal-nightly/bin
-export PYTHONPATH=/vol/toolkit/cogimon-minimal-nightly/lib/python2.7/site-packages/:$PYTHONPATH
